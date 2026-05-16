fc7e5266e948ce01dfd55d00704847c60e9799ec
Daniel Estrada <danielebeb01@gmail.com>
2026-05-16 15:06:05 -0500

    first commit

---
+++ 01_basic/01_print.py
@@ -0,0 +1,10 @@
+print ("hola Mundo")
+#Tambien funciona con una sola columna
+print('¡Hola Mundo!')
+#Puedo imprimir varios parametroa al tiempo, esto me permite mostrar varias cosas. El separador que utiliza por defecto Python es Espacio
+print('hola','mundo','del','Phyton')
+#Para colocar un separador especifico debo de utilizar la función 'sep'
+print('hola','mundo','del','Phyton', sep ="_")
+#Para evitar el saldo de linea se debe de realizar lo siguiente
+print('esto se imprime', end =' ')
+print('En una Linea')
